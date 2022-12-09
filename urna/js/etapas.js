6582bda313a0b76cc2133ed7e49a8b18bb1a5047
jacrifla <jaquelini_cris@hotmail.com>
2022-12-09 03:45:06 -0300

    urna eletronica

---
+++ urna/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Diego Alves',
+                partido: 'Partido do Cidadão',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Lara Lima',
+                partido: 'Partido Justos',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Pedro Miguel Alves',
+                partido: 'Partido Farofa',
+                vice: 'Vitor Hugo Viana',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Julia Nunes',
+                partido: 'Partido da Garoa',
+                vice: 'Nicolas da Conceição',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
