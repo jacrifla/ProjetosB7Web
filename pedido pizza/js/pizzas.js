cbb730c16b8fa860ab627fdc2d6de950eeecab97
jacrifla <jaquelini_cris@hotmail.com>
2022-12-20 22:47:54 -0300

    css

---
+++ pedido pizza/js/pizzas.js
@@ -0,0 +1,58 @@
+let pizzaJson = [
+    {
+        id:1, 
+        name:'Mussarela', 
+        img:'images/pizza.png', 
+        price: 35.50, 
+        sizes: ['200g', '550g', '850g'], 
+        description: 'Mussarela, Molho de Tomate e Tomate'
+    },
+    {
+        id:2, 
+        name:'Calabresa', 
+        img:'images/pizza2.png', 
+        price: 35.50, 
+        sizes: ['200g', '550g', '850g'], 
+        description: 'Calabresa, Cebola e Molho de Tomate'
+    },
+    {
+        id:3, 
+        name:'Portuguesa', 
+        img:'images/pizza3.png', 
+        price: 42.50, 
+        sizes: ['200g', '550g', '850g'], 
+        description: 'Mussarela, Preseunto Picado, Ervilha, Ovo, Cebola e Molho de Tomate'
+    },
+    {
+        id:4, 
+        name:'Quatro Queijos', 
+        img:'images/pizza4.png', 
+        price: 53.50, 
+        sizes: ['200g', '550g', '850g'], 
+        description: 'Mussarela, Parmesão, Gorgonzola, Requeijão Cremoso e Molho de Tomate'
+    },
+    {
+        id:5, 
+        name:'Toscana', 
+        img:'images/pizza5.png', 
+        price: 40.20, 
+        sizes: ['200g', '550g', '850g'], 
+        description: 'Mussarela, Calabresa, Ervilha, Palmito e Molho de Tomate'
+    },
+    {
+        id:6, 
+        name:'Marguerita', 
+        img:'images/pizza6.png', 
+        price: 41.10, 
+        sizes: ['200g', '550g', '850g'], 
+        description: 'Mussarela, Molho de Tomate, Manjericão fresco e mais Molho de Tomate'
+    },
+    {
+        id:7, 
+        name:'Chocolate', 
+        img:'images/pizza7.png', 
+        price: 50.25, 
+        sizes: ['200g', '550g', '850g'], 
+        description: 'Chocolate e M&M'
+    }
+]
