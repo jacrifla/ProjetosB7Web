b1fcbcd7d8aa15464c69a58fc760428a4262138c
jacrifla <jaquelini_cris@hotmail.com>
2023-03-02 16:22:47 -0300

    continuacao

--- quiz/script.js
+++ quiz/script.js
@@ -2,13 +2,20 @@
 
 // Exibir primeiro as questões
 let currentQuestion = 0
-
+let correctAnswers = 0
 showQuestion()
+
+// Eventos
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent)
 
 // Funções
 function showQuestion() {
     if (questions[currentQuestion]) {
         let q = questions[currentQuestion] // para ficar menor o comando
+
+        let pct = Math.floor((currentQuestion / questions.length) * 100)
+
+        document.querySelector('.progress--bar').style.width = `${pct}%` // mover a barra pela porcentagem
         
         document.querySelector('.scoreArea').style.display = 'none'
         document.querySelector('.questionArea').style.display = 'block'
@@ -18,11 +25,53 @@ function showQuestion() {
         
         let optionsHtml = ''
         for (let i in q.options) {
-            optionsHtml += `<div class="option"><span>${parseInt(i) + 1}</span> ${q.options[i]}</div>`
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i) + 1}</span> ${q.options[i]}</div>`
         }
-        document.querySelector('.options').innerHTML = optionsHtml
+        document.querySelector('.options').innerHTML = optionsHtml // vai inserir na tela
 
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent)
+        })
     } else {
-        // Acabaram as questões
+        finishQuiz()
     }
+}
+
+function optionClickEvent(e) {
+    let clickedOption = parseInt(e.target.getAttribute('data-op'))
+    if (questions[currentQuestion].answer === clickedOption) { // Para ver c acertou
+        correctAnswers++   
+    }
+
+    currentQuestion++
+    showQuestion()
+}
+
+function finishQuiz() {
+    let points = Math.floor((correctAnswers / questions.length) * 100)
+    if (points < 30) {
+        document.querySelector('.scoreText1').innerHTML = 'Ta ruin hein?!'
+        document.querySelector('.scorePct').style.color = '#ff0000'
+    } else if (points >= 30 && points < 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Muito bom!'
+        document.querySelector('.scorePct').style.color = '#ffff00'
+    } else if (points >= 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Parabéns!'
+        document.querySelector('.scorePct').style.color = '#0d630d'
+    }
+    
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers}.`
+
+    document.querySelector('.scoreArea').style.display = 'block'
+    document.querySelector('.questionArea').style.display = 'none'
+    document.querySelector('.progress--bar').style.width = `100%`
+
+
+}
+
+function resetEvent() {
+    correctAnswers = 0
+    currentQuestion = 0
+    showQuestion()
 }
